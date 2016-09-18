6bb52291337e2c7bf1bea25aec8ac8084ab4b308
David Forman <djforman@gmail.com>
2016-09-18 13:54:43 -0400

    Added files

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+// ===============================================================================
+// DATA
+// Will hold the info about the friend
+// ===============================================================================
+
+
+var friendArray = [
+	{
+		Name: "Ahmed",
+		Photo: "afhaque89@gmail.com",
+		Scores: [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10
+		]
+
+
+	}
+];
+
+// Note how we export the array. This makes it accessible to other files using require. 
+module.exports = friendArray; 
